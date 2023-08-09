48d89653eec31a0e19b1aaf4ba9f4f94c8ae501a
ivk <ivan.keynes@bbconsult.co.uk>
2023-08-09 13:16:59 +0200

    added model for MX Records

---
+++ MXRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace MasterResolver9000
+{
+    public class MXRecord
+    {
+        public int Preference { get; set; }
+        public string InputMail { get; set; }
+        public string MailServer { get; set; }
+        public string DnsServer { get; set; }
+    }
+}
